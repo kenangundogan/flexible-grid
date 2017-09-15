06c5569dc7c657fe921bfa0bffce74a72fcad4ae
Kenan Gündoğan <kenangundogann@gmail.com>
2017-09-15 23:51:26 +0300

    Update travis.rb

--- test/travis.rb
+++ test/travis.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-result = `sass src/sass/flexible-grid.scss built.css`
+result = `sass src/sass/flexible-grid.scss flexible-grid.css`
 raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('built.css')
-puts "Regular compile worked successfully"
+raise "When compiled the module should output some CSS" unless File.exists?('flexible-grid.css')
+puts "SASS compile was successful."
