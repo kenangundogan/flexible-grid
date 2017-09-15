be9640e92c6d4fb212f20e9fcc3e4482c8daa337
Kenan Gündoğan <kenangundogann@gmail.com>
2017-09-15 23:35:39 +0300

    Delete travis.rb

--- travis.rb
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env ruby
-result = `sass style.scss built.css`
-raise result unless $?.to_i == 0
-raise "When compiled the module should output some CSS" unless File.exists?('built.css')
-puts "Regular compile worked successfully"
